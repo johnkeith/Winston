f300164b16400b7758266797449ee681eddc2bf6
John Keith <johnkeith@outlook.com>
2015-08-14 22:19:04 -0400

    switch links to load from tapping the name or the white space; option slide out working on web and ios

--- www/js/inAppBrowserEventHandler.js
+++ www/js/inAppBrowserEventHandler.js
@@ -3,8 +3,11 @@ document.onclick = function (e) {
   var element = e.target || e.srcElement;
   	console.log(element);
 
-  if (element.className == 'item-source-link ng-binding') {
-    window.open(element.getAttribute('href'), "_blank", "location=yes");
+  if (element.className == 'item-content activated' && element.getAttribute('ng-href') != null) {
+    window.open(element.getAttribute('ng-href'), "_blank", "location=yes");
     return false; 
+  } else if (element.className == 'item-source-link ng-binding' && element.getAttribute('href') != null) {
+  	window.open(element.getAttribute('href'), "_blank", "location=yes");
+  	return false;
   }
 };
