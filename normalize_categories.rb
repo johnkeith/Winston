64f5f390d77bc4e779903307279005cc3fe3f7bf
John Keith <johnkeith@outlook.com>
2015-08-30 23:00:16 -0400

    working to normalize recipe categories; better icons

--- normalize_categories.rb
+++ normalize_categories.rb
@@ -1,10 +1,48 @@
 require 'pry'
 require 'json'
 
-ssk_data = JSON.parse File.read('ssk_scrapped.json')
-lal_data = JSON.parse File.read('lal_scrapped.json')
-poy_data = JSON.parse File.read('poy_scrapped.json')
-dd_data = JSON.parse File.read('dd_scrapped.json')
-binding.pry
+puts "\e[H\e[2J"
 
+data = JSON.parse File.read('combined.json')
 
+valid_categories = ['vegan', 'vegetarian', 'gluten-free', 'seafood', 'beef', 'pork', 'chicken']
+
+data.each_with_index do |d, idx|
+	if d['cuisineType'].map{ |i| valid_categories.include?(i) }.uniq.include?(false)
+		puts "\e[H\e[2J"
+
+		puts "-------------------------------------------------"
+		puts "Title: #{d['recipeTitle']} - (#{idx} of #{data.size})"
+		puts "-------------------------------------------------"
+
+		puts ''
+		puts "-------------------------------------------------"
+		puts "Ingredients: "
+		puts "-------------------------------------------------"
+		d['ingredients'].each do |i|
+			puts i
+		end
+
+		puts ''
+		puts "-------------------------------------------------"
+		puts "Categories: #{d['cuisineType']}"
+		puts "-------------------------------------------------"
+
+		puts ''
+		puts "New category names:"
+
+		names = gets.chomp
+
+		data.delete_at(idx) if names == 'd'
+
+		next if names == 'n' || names == 'd'
+
+		names = names.split(",").map { |n| n.gsub(' ', '') }
+
+		d['cuisineType'] = names
+
+		File.open('combined.json', 'w') do |f|
+			f.write JSON.pretty_generate(data)
+		end
+	end
+end
