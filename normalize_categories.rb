ac36d9599634ce330b82bd15b3616123b2e5b8ca
John Keith <johnkeith@outlook.com>
2015-09-05 14:19:43 -0500

    fix veggie recipe titles/links

--- normalize_categories.rb
+++ normalize_categories.rb
@@ -5,44 +5,66 @@ puts "\e[H\e[2J"
 
 data = JSON.parse File.read('combined.json')
 
-valid_categories = ['veggie', 'vegan', 'vegetarian', 'gluten-free', 'seafood', 'beef', 'pork', 'chicken']
+count = 0
 
-data.each_with_index do |d, idx|
-	if d['cuisineType'].map{ |i| valid_categories.include?(i) }.uniq.include?(false)
-		puts "\e[H\e[2J"
-
-		puts "-------------------------------------------------"
-		puts "Title: #{d['recipeTitle']} - (#{idx} of #{data.size})"
-		puts "-------------------------------------------------"
-
-		puts ''
-		puts "-------------------------------------------------"
-		puts "Ingredients: "
-		puts "-------------------------------------------------"
-		d['ingredients'].each do |i|
-			puts i
-		end
-
-		puts ''
-		puts "-------------------------------------------------"
-		puts "Categories: #{d['cuisineType']}"
-		puts "-------------------------------------------------"
-
-		puts ''
-		puts "New category names:"
-
-		names = gets.chomp
-
-		data.delete_at(idx) if names == 'd'
-
-		next if names == 'n' || names == 'd'
-
-		names = names.split(",").map { |n| n.gsub(' ', '') }
-
-		d['cuisineType'] = names
-
-		File.open('combined.json', 'w') do |f|
-			f.write JSON.pretty_generate(data)
-		end
+data.each do |d|
+	if d['sourceHref'].include?('vegetarian')
+		binding.pry
 	end
 end
+
+puts count
+# data.map! do |d|
+# 	if d['sourceName'] == 'Damn Delicious'
+# 		d['sourceHref'] = 'http://damndelicious.net' + d['sourceHref']
+# 		d
+# 	else
+# 		d
+# 	end
+# end
+
+# File.open('combined.json', 'w') do |f|
+# 	f.write JSON.pretty_generate(data)
+# end
+
+# valid_categories = ['veggie', 'vegan', 'vegetarian', 'gluten-free', 'seafood', 'beef', 'pork', 'chicken']
+
+# data.each_with_index do |d, idx|
+# 	if d['cuisineType'].map{ |i| valid_categories.include?(i) }.uniq.include?(false)
+# 		puts "\e[H\e[2J"
+
+# 		puts "-------------------------------------------------"
+# 		puts "Title: #{d['recipeTitle']} - (#{idx} of #{data.size})"
+# 		puts "-------------------------------------------------"
+
+# 		puts ''
+# 		puts "-------------------------------------------------"
+# 		puts "Ingredients: "
+# 		puts "-------------------------------------------------"
+# 		d['ingredients'].each do |i|
+# 			puts i
+# 		end
+
+# 		puts ''
+# 		puts "-------------------------------------------------"
+# 		puts "Categories: #{d['cuisineType']}"
+# 		puts "-------------------------------------------------"
+
+# 		puts ''
+# 		puts "New category names:"
+
+# 		names = gets.chomp
+
+# 		data.delete_at(idx) if names == 'd'
+
+# 		next if names == 'n' || names == 'd'
+
+# 		names = names.split(",").map { |n| n.gsub(' ', '') }
+
+# 		d['cuisineType'] = names
+
+		# File.open('combined.json', 'w') do |f|
+		# 	f.write JSON.pretty_generate(data)
+		# end
+# 	end
+# end
